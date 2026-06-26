ef0e35005f19e34e33edca17f0b50cc63df92f75
hgrondin <henri.grondin100@gmail.com>
2026-06-26 14:14:47 -0400

    commit test hello world

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,20 @@
-def test_app_factory_creates_app():
-    from app import create_app
+# def test_app_factory_creates_app():
+#     from app import create_app
 
-    app = create_app()
+#     app = create_app()
 
-    assert app is not None
+#     assert app is not None
 
 
-def test_homepage_redirects_to_login():
-    from app import create_app
+# def test_homepage_redirects_to_login():
+#     from app import create_app
 
-    app = create_app()
-    client = app.test_client()
-    response = client.get("/")
+#     app = create_app()
+#     client = app.test_client()
+#     response = client.get("/")
 
-    assert response.status_code == 302
+#     assert response.status_code == 302
+
+
+def test_pipeline():
+    print("Hello, World!")
